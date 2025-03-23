c1fce1560331413209909197c1d5fbd811f3020a
萧上晋 <84136399+Shangjin-Xiao@users.noreply.github.com>
2025-03-23 13:52:33 +0800

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/shangjin/yiyan/MainActivity.kt
+++ android/app/src/main/kotlin/com/shangjin/yiyan/MainActivity.kt
@@ -1,6 +1,17 @@
 package com.shangjin.yiyan
 
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
-} 
+
+    companion object {
+        init {
+            System.loadLibrary("sqlite3")
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
