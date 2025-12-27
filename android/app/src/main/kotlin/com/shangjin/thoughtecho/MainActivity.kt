59b86c67acef8b52dca8dd3d2aa6c4f4f3526e2c
上晋 <84136399+Shangjin-Xiao@users.noreply.github.com>
2025-12-27 01:46:05 +0000

    将MainActivity类从FlutterActivity更改为FlutterFragmentActivity，以支持片段管理

--- android/app/src/main/kotlin/com/shangjin/thoughtecho/MainActivity.kt
+++ android/app/src/main/kotlin/com/shangjin/thoughtecho/MainActivity.kt
@@ -4,10 +4,10 @@ import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.WindowManager
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 import io.flutter.embedding.engine.FlutterEngine
 
-class MainActivity : FlutterActivity() {
+class MainActivity : FlutterFragmentActivity() {
 
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
